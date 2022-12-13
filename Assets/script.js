bfda0db323e7fc1ca4f849d93494917a96d31cfa
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2022-12-12 22:01:42 -0500

    added starter code for css html and js

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+//when i click the start buttton I want to be asked a series of questions
+// when i click start I want a timer to start and display in the quiz
+//I want right and wrong answers to dynamically update
+//I want the recorded totals to be displayed in a leaderboard stored in local storage
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+var myscore = "";
+const quesArray = [];
+const answerKey = [];
+
+function startquiz() {}
+
+function questiongenerater() {}

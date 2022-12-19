dc8248af38b106803e136a0350a1835ecff92ead
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2022-12-18 20:15:04 -0500

    added javascript and css and html

--- Assets/script.js
+++ Assets/script.js
@@ -8,75 +8,97 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
-const questarray = [
-  "Which of these choices are not a Javascript data type? <br> 1.Boolean 2.String 3.Numbers 4.Space ",
-  "What year was Javascript invented? <br> 1.1995 2.1900 3.2001 4.1988",
-  "Who is the father of JavaScript? <br> 1.Brendan Eich 2.Brendan Fraiser 3.Lebron James 4.Bill Gates",
-  "What was the original name of Netscape's JavaScript engine? <br> 1.Bananbox 2.Spidermonkey 3.Donkeypot 4.Netscape",
-  "Which of the following is not an operator? <br> 1.Addition 2.Division 3.Extraction 4.Assignment",
-];
+const startingminutes = 3;
+let time = startingminutes * 60;
 
-const answerkey = [4, 1, 1, 2, 3];
+const countdownEl = document.getElementById("countdown");
+var i = 0;
+var w = 0;
 
 $("#questions").hide();
+
+const questarray = [
+  "Space is not a Javascript data type?",
+  "Javascript was invented in 1999.",
+  "Javascript is named after Java.",
+  "SpiderMonkey was the original name of Netscape's JavaScript engine.",
+  "Assignment is not an operator of Javascript.",
+  "Brendan Eich is the father of JavaScript.",
+  "Javascript only works on Windows computers.",
+];
+
+const answerkey = ["TRUE", "FALSE", "TRUE", "TRUE", "FALSE", "TRUE", "FALSE"];
+
+document.getElementById("quiz").innerHTML = questarray[i];
 
 $(document).ready(function () {
   $("#start").click(function () {
     $("#questions").toggle();
-    timerCountdown();
-    setInterval(timerCountdown, 1000);
   });
 });
 
-const startingminutes = 5;
-let time = startingminutes * 60;
-
-const countdownEl = document.getElementById("countdown");
+$("#start").on("click", function () {
+  timerCountdown();
+  var interval = setInterval(timerCountdown, 1000);
+  $(this).prop("disabled", true);
+  if (time === 00) {
+    console.log("out of time");
+    clearInterval(interval);
+  }
+});
 
 function timerCountdown() {
   const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-
+  var seconds = time % 60;
   seconds = seconds < 1 ? "0" + seconds : seconds;
 
   countdownEl.innerHTML = `${minutes}: ${seconds}`;
   time--;
 }
+
 document
   .getElementById("questions")
   .addEventListener("submit", function (event) {
     event.preventDefault();
   });
-var i = 0;
-document.getElementById("quiz").innerHTML = questarray[i];
+
 function cycle() {
   var inputVal = document.querySelectorAll("input")[0].value;
-  while (inputVal == answerkey[i]) {
+
+  console.log(inputVal);
+  if (inputVal === answerkey[i]) {
+    i++;
+    console.log(i);
+    document.getElementById("quiz").innerHTML = questarray[i];
+  } else if (i >= 5) {
+    $("#countdown").hide();
+    $("label").hide();
+    document.getElementById("quiz").innerHTML = "PLEASE ENTER YOUR INITIALS!";
+    var score = "";
+    time = 00;
+  } else {
+    alert("You are incorrect! Try again!");
+    score = ++w;
+    var total = -w * 5;
+    console.log(score);
+    time -= 30;
     ++i;
     document.getElementById("quiz").innerHTML = questarray[i];
-  }
-  if (i == 5) {
-    time = 0;
-    $("#countdown").hide();
-    document.getElementById("quiz").innerHTML = "GOOD WORK!";
-    return false;
+    var totalscore = total + 100;
   }
 }
 
-// $.each(questarray, function (i, value) {
-// $("#questions").append(i + ":" + value);
+//  added color change text for styling purposes
+const checkFilled = (input) => {
+  if (!input.value) {
+    input.style.backgroundColor = "babyblue";
+  } else {
+    input.style.backgroundColor = "white";
+  }
+};
 
-// added color change text for styling purposes
-// const checkFilled = (input) => {
-//   if (!input.value) {
-//     input.style.backgroundColor = "yellow";
-//   } else {
-//     input.style.backgroundColor = null;
-//   }
-// };
-
-// const input = document.querySelector("input");
-// input.onchange = (e) => {
-//   checkFilled(e.target);
-// };
-// checkFilled(input);
+const input = document.querySelector("input");
+input.onchange = (e) => {
+  checkFilled(e.target);
+};
+checkFilled(input);

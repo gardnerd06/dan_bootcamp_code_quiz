8c2e17faf7628b267e8c028d88eedffe65c00a33
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2022-12-20 00:37:25 -0500

    added comments to js

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -8,14 +8,15 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+// added variables to reference throughout script
 const startingminutes = 3;
 let time = startingminutes * 60;
 var username = document.querySelectorAll("user");
 const countdownEl = document.getElementById("countdown");
 var i = 0;
-
+// add jquery to hide some text by default
 $("#questions").hide();
-
+// made arrays to store questions and answers for quiz
 const questarray = [
   "Space is not a Javascript data type?",
   "Javascript was invented in 1999.",
@@ -39,19 +40,18 @@ const answerkey = [
 ];
 
 document.getElementById("quiz").innerHTML = questarray[i];
-
+// added toggle functionality for enhanced ui
 $(document).ready(function () {
   $("#start").click(function () {
     $("#questions").toggle();
   });
 });
-
+// added timer for countdown when user begins quiz
 $("#start").on("click", function () {
   timerCountdown();
   var interval = setInterval(timerCountdown, 1000);
   $(this).prop("disabled", true);
   if (time === 00) {
-    console.log("out of time");
     clearInterval(interval);
   }
 });
@@ -64,7 +64,7 @@ function timerCountdown() {
   countdownEl.innerHTML = `${minutes}: ${seconds}`;
   time--;
 }
-
+// added event listeners to block submit functions on form elements
 document
   .getElementById("questions")
   .addEventListener("submit", function (event) {
@@ -74,13 +74,11 @@ document.getElementById("user").addEventListener("submit", function (event) {
   event.preventDefault();
   retrieve();
 });
-
+// created a function to cycle through the questionas and answers to give user true or false questions
 function cycle() {
   var inputVal = document.querySelectorAll("input")[0].value;
   var w = 0;
-  var c = 0;
-  var score = "";
-  console.log(inputVal);
+
   if (inputVal === answerkey[i]) {
     ++i;
     document.getElementById("quiz").innerHTML = questarray[i];
@@ -116,11 +114,11 @@ input.onchange = (e) => {
   checkFilled(e.target);
 };
 checkFilled(input);
+// added code to store and retrieve and append data from local storage
 
 function store() {
   var name = document.getElementById("player").value;
   localStorage.setItem("username", name);
-  console.log(name);
 }
 
 function retrieve() {
@@ -128,8 +126,6 @@ function retrieve() {
   const quez = localStorage.getItem("totalques");
   const incorrect = localStorage.getItem("wrong");
   var final = !incorrect - -6 + "OUT OF" + quez;
-  console.log(incorrect);
-  console.log(final);
   document.getElementById("leader").innerHTML = leader;
   document.getElementById("final").innerHTML = final;
 }
